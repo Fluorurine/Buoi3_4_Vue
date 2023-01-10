a8b90f585a2cfe35f640c55ff8ae16128bab9757
Fluorurine <votruong830430073@gmail.com>
2023-01-10 21:26:24 +0700

    Hiển thị danh sách các contact lấy về từ server

---
+++ src/services/contact.service.js
@@ -0,0 +1,23 @@
+import createApiClient from "./api.service.js";
+const ContactService = {};
+
+const api = createApiClient("/api/contacts");
+ContactService.getAll = async () => {
+  return (await api.get("/")).data;
+};
+ContactService.create = async (data) => {
+  return (await api.post("/", data)).data;
+};
+ContactService.deleteAll = async () => {
+  return (await api.delete("/")).data;
+};
+ContactService.get = async (id) => {
+  return (await api.get(`/${id}`)).data;
+};
+ContactService.update = async (id, data) => {
+  return (await api.put(`/${id}`, data)).data;
+};
+ContactService.get = async (id) => {
+  return (await api.delete(`/${id}`)).data;
+};
+export default ContactService;
